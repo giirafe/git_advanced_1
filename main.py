43909dae0934afab9cb610abc74499e100547463
giirafe <seojune408@gmail.com>
2023-10-18 18:26:48 +0900

    Reset with given boilerplate

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from typing import List
-
-# Function to filter even numbers from a list
+# Skeleton code for even_list
 
 
 def even_list(int_list: List[int]) -> List[int]:
     """
-    Determines if a number is even and returns a list of even integers.
+    Determines if a number is even and return an even list.
     Args:
-        int_list: A list of integers.
+        int_list: A list of integer.
     Returns:
         A list of even integers.
     """
-    even_numbers = [x for x in int_list if x % 2 == 0]
-    return even_numbers
+    # TODO: Implement even_list
 
-# Function to calculate the sum of the squares of even numbers in a list
+    pass
+
+# Skeleton code for sum_of_squares_of_even
 
 
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
@@ -25,10 +25,8 @@ def sum_of_squares_of_even(even_int_list: List[int]) -> int:
     Returns:
         The sum of the squares of all even numbers in the list.
     """
-    # sum_of_squares = sum(x ** 2 for x in even_int_list)
-    # return sum_of_squares
+    # TODO: Implement sum_of_squares_of_even
     pass
-
 # Main function
 
 
